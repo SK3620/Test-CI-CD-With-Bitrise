4e8f9e21200c8cde94d0270f58d70ffdd8184fc6
SK3620 <k.n.t11193620@gmail.com>
2025-05-13 22:05:17 +0900

    initial commit

---
+++ TestCICDWithBitrise/TestCICDWithBitriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCICDWithBitriseApp.swift
+//  TestCICDWithBitrise
+//
+//  Created by 鈴木 健太 on 2025/05/13.
+//
+
+import SwiftUI
+
+@main
+struct TestCICDWithBitriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestCICDWithBitriseTests/TestCICDWithBitriseTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestCICDWithBitriseTests.swift
+//  TestCICDWithBitriseTests
+//
+//  Created by 鈴木 健太 on 2025/05/13.
+//
+
+import Testing
+@testable import TestCICDWithBitrise
+
+struct TestCICDWithBitriseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
